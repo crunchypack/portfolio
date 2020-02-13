550f0e527189edb7e2956e7a63ba34c7b580812c
Simon <simon.wolf.lobo@gmail.com>
2020-02-13 18:00:48 -0500

    Added carousel and cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,18 @@
 import React from "react";
+import CardInfo from "./CardInfo";
+
+function Card({ item, click }) {
+  return (
+    <div className="d-inline-block s-card" onClick={e => click(item)}>
+      <img className="s-card-image" src={item.imgSrc} alt={item.imgSrc} />
+      {item.selected && (
+        <CardInfo
+          title={item.title}
+          subTitle={item.subTitle}
+          link={item.link}
+        />
+      )}
+    </div>
+  );
+}
+export default Card;
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,3 +1,77 @@
 import React from "react";
 
-class Carousel extends React.Component {}
+import Axio from "../assets/images/Axio.png";
+import WhereToWatch from "../assets/images/WhereTo.png";
+import Fantasy from "../assets/images/Fantasy.png";
+import Card from "./Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: "Axio",
+          subTitle: "Cyber Security",
+          imgSrc: Axio,
+          link: "https://axio.com/",
+          selected: false
+        },
+        {
+          id: 1,
+          title: "Where To Watch",
+          subTitle: "Vue school project",
+          imgSrc: WhereToWatch,
+          link: "https://lobonode.ddns.net/WhereToWatch/",
+          selected: false
+        },
+        {
+          id: 2,
+          title: "Fantasy Football",
+          subTitle: "Wordpress school project",
+          imgSrc: Fantasy,
+          link: "https://lobonode.ddns.net/wordpress/",
+          selected: false
+        }
+      ]
+    };
+  }
+
+  handleCardClick = (id, card) => {
+    let items = [...this.state.items];
+    items[id].selected = items[id].selected ? false : true;
+
+    items.forEach(item => {
+      if (item.id !== id) {
+        item.selected = false;
+      }
+    });
+
+    this.setState({ items });
+  };
+
+  makeItems = items => {
+    return items.map(item => {
+      return (
+        <Card
+          item={item}
+          click={e => this.handleCardClick(item.id, e)}
+          key={item.id}
+        />
+      );
+    });
+  };
+  render() {
+    return (
+      <Container fluid={true}>
+        <Row className="justify-content-around">
+          {this.makeItems(this.state.items)}
+        </Row>{" "}
+      </Container>
+    );
+  }
+}
+export default Carousel;
